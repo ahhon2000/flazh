22ed23e5a0f175c5063e1f8789eb4d976682bfc2
ahhon <polygon2000x@gmail.com>
2021-09-19 02:22:24 +0700

    a minor requirejs module name change

--- client/core/Flazh.js
+++ client/core/Flazh.js
@@ -1,11 +1,14 @@
 
-define(['lib/socket.io.min.js', 'core/Stack'], function(io, Stack) {
+define(['lib/socket.io.min', 'core/Stack'], function(io, Stack) {
     return {
         Flazh: class {
             constructor() {
                 let sta = new Stack.Stack();
                 this.stack = sta;
+
                 let sock = io('http://127.0.0.1:5000'); // TODO rmme
+                this.sock = sock;
+
                 sock.emit('my_message', 'test334');
                 console.log('original Flazh');
                 $('body').append('<br>jQuery works, too!');
