4fda48ce3e62a45f14ad7325759c077bbd4305a8
ahhon <polygon2000x@gmail.com>
2021-09-18 19:28:38 +0700

    Flazh.js

---
+++ client/Flazh.js
@@ -0,0 +1,12 @@
+
+define(['Stack'], function(Stack) {
+    return {
+        Flazh: class {
+            constructor() {
+                let sta = new Stack.Stack();
+                this.stack = sta;
+                console.log(io); // TODO rmme
+            }
+        },
+    }
+});
