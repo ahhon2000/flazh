4d94c28489cce080f5818b327f793dd220bdcccc
ahhon <polygon2000x@gmail.com>
2021-09-21 02:13:05 +0700

    flazh_server.py: unix socket support

--- server/flazh_server.py
+++ server/flazh_server.py
@@ -3,11 +3,22 @@ import sys, pathlib; d = pathlib.Path(__file__).resolve()
 while d.parent != d and not (d / 'Common.py').exists(): d = d.parent
 sys.path.count(str(d)) or sys.path.insert(0,str(d)); from Common import inclPath
 
-from FlazhServer import FlazhServer, DEFAULT_PORT
+from pathlib import Path
+
+from FlazhServer import FlazhServer, DEFAULT_PORT, DEBUG
+
+SOCKET_PATH = Path('/tmp/flazh.sock')
 
 server = FlazhServer()
 app = server.app
 
 if __name__ == '__main__':
     import eventlet
-    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', DEFAULT_PORT)), app)
+    l = None
+    if DEBUG:
+        l = eventlet.listen(('127.0.0.1', DEFAULT_PORT))
+    else:
+        sockPath = str(SOCKET_PATH)
+        l = eventlet.listen(sockPath, eventlet.wsgi.socket.AF_UNIX)
+
+    eventlet.wsgi.server(l, app)
