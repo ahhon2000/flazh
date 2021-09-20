39498bdf29d515cb6a07b0b136f5b7f44f31341c
ahhon <polygon2000x@gmail.com>
2021-09-21 02:23:55 +0700

    flazh_server.py: listen on 5491 when not debugging

--- server/flazh_server.py
+++ server/flazh_server.py
@@ -8,6 +8,8 @@ from pathlib import Path
 from FlazhServer import FlazhServer, DEFAULT_PORT, DEBUG
 
 SOCKET_PATH = Path('/tmp/flazh.sock')
+USE_UNIX_SOCKET = False
+PORT = 5491
 
 server = FlazhServer()
 app = server.app
@@ -15,10 +17,12 @@ app = server.app
 if __name__ == '__main__':
     import eventlet
     l = None
-    if DEBUG:
-        l = eventlet.listen(('127.0.0.1', DEFAULT_PORT))
-    else:
+    if USE_UNIX_SOCKET:
         sockPath = str(SOCKET_PATH)
         l = eventlet.listen(sockPath, eventlet.wsgi.socket.AF_UNIX)
+    elif DEBUG:
+        l = eventlet.listen(('127.0.0.1', DEFAULT_PORT))
+    else:
+        l = eventlet.listen(('127.0.0.1', PORT))
 
     eventlet.wsgi.server(l, app)
