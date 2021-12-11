d9f1373cb8d2a5a3863fc896d97006816ee89fd6
ahhon <polygon2000x@gmail.com>
2021-12-11 17:57:12 +0400

    removed boilerplate code from the root __init__

--- __init__.py
+++
@@ -1,6 +0,0 @@
-try:from pathlib import Path as P;R=P.resolve;E=P.exists; F = R(P(__file__));\
-    L = lambda p: p / 'cfg.py'; from handyPyUtil import inclPath
-except: O=open(R(next(filter(E,map(L,F.parents))))); exec(O.read()); O.close()
-
-inclPath('..')
-import arraySocket
